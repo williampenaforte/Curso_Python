d19b2976ac6856fd00208b8fa5c69b591d613a80
williampenaforte <williampenaforte@gmail.com>
2022-11-09 19:17:12 -0300

    003

--- ex002.py
+++ ex002.py
@@ -1,5 +1,4 @@
 # exercícios python
 # william pena forte 09-11-2022
 msg = input('Digite seu nome')
-print('É um prazer te conhecer',msg,'!')
-
+print('É um prazer te conhecer, {}!'.format(msg))
---
+++ ex003,2.py
@@ -0,0 +1,12 @@
+# exercícios python
+# william pena forte 09-11-2022
+# apos estudar aula 06 do curso https://www.youtube.com/playlist?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6
+
+
+v1 = int(input('Digite um numero'))
+v2 = int(input('Digite outro numero'))
+
+resultado_soma = v1 + v2
+
+print('a soma entre',v1,'e',v2,'é igual a',resultado_soma)
+

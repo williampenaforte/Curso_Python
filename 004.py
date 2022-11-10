2707bf4c47011ae8e6815f7c0dc2b0f4cddee876
williampenaforte <williampenaforte@gmail.com>
2022-11-10 19:26:47 -0300

    003

--- 004.py
+++ 004.py
@@ -4,3 +4,6 @@
 a = input('digite algo: ')
 
 print('O tipo primitivo desse valor é {}'.format(type(a)))
+print('vc precinou barra de espaço {}'.format(a.isspace()))
+print('vc digitou um numero {}'.format(a.isnumeric()))
+print('vc digitou um numero decimal {},{}'.format(a.isdecimal(),(a)))

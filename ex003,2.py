a34fc965bf7c72bd4d5edfbf0e32eb0df5ce4220
williampenaforte <williampenaforte@gmail.com>
2022-11-10 07:45:39 -0300

    003

--- ex003,2.py
+++ ex003,2.py
@@ -3,10 +3,12 @@
 # apos estudar aula 06 do curso https://www.youtube.com/playlist?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6
 
 
-v1 = int(input('Digite um numero'))
-v2 = int(input('Digite outro numero'))
+v1 = int(input('Digite um numero: '))
+v2 = int(input('Digite outro numero: '))
 
-resultado_soma = v1 + v2
+print(type(v1))
+print(type(v2))
+#resultado_soma = v1 + v2
 
-print('a soma entre',v1,'e',v2,'é igual a',resultado_soma)
+#print('a soma entre',v1,'e',v2,'é igual a',resultado_soma)
 

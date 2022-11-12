13ed2607905c6ec823b131efd474e1d62ad4dbca
williampenaforte <williampenaforte@gmail.com>
2022-11-12 08:49:38 -0300

    007

--- 007.py
+++ 007.py
@@ -2,7 +2,6 @@
 # william pena forte 11-11-2022
 
 # fazer um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor.
-# exp numero 8 -- antecessor 7 , num 8 , sucessor 9
 
 # desenvolva um programa que leia as mduas notas de um aluno, calcule e mostre sua media.
 n1 = float(input('Qual a primeira nota do aluno? '))

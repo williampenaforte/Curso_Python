c9432c3f2ea0ab75a2631f2de027c70b55d06e5e
williampenaforte <williampenaforte@gmail.com>
2022-11-12 08:46:53 -0300

    006

--- 006.py
+++ 006.py
@@ -4,17 +4,10 @@
 # fazer um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor.
 # exp numero 8 -- antecessor 7 , num 8 , sucessor 9
 
-# crie um altoritimo que leia um numero e mostre o seu dobro, triplo e sua raiz quadrada.
+# crie um algoritimo que leia um numero e mostre o seu dobro, triplo e sua raiz quadrada.
 n = float(input('digite um numero: '))
 
-d = n * 2
-t = n * 3
-r = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n,(n*2)))
+print('O triplo de {} vale {}.\nA rais quadrade de {} e igual {:.2f}.'.format(n,(n*3),n,pow(n,(1/2))))
 
-print('O dobro de {} vale {}'.format(n,d))
-print('O triplo de {} vale {}'.format(n,t))
-print('A Rais quadrada de {} e igual a {:.0f}'.format(n,r))
-print('A Raiz quadrada de {} e igual a {:.1f}'.format(n,r))
-print('A Raiz quadrada de {} e igual a {:.2f}'.format(n,r))
-print('A Raiz quadrada de {} e igual a {:.3f}'.format(n,r))
-print('A Rais quadrada de {} e igual a {}'.format(n,r))
+

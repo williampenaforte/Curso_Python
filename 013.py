7637358b31e623ba30efae5bc1de8c262752efd6
williampenaforte <williampenaforte@gmail.com>
2022-11-23 17:24:11 -0300

    231122

---
+++ 013.py
@@ -0,0 +1,10 @@
+#exercicios py
+#william 23-11-22
+
+# faça um algoritimo que leia o salario de um funcionario e mostre seu novo salario com 15% de aumento.
+
+salario = float(input('Qual o salario do funcionario? R$'))
+
+n = salario + (salario*15/100)
+
+print('O novo sario do funcionari e R${:.2f}'.format(n))

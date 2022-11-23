dd0c067c6c32011e49f0a266ea808ea83d6d031b
williampenaforte <williampenaforte@gmail.com>
2022-11-23 17:33:00 -0300

    atualziando apos agluns dias sem estuda. voltamos

--- 000.py
+++ 000.py
@@ -1,7 +1,7 @@
-#curso_python
-#william pena forte
-#09-11-2022
+#curso_python, william pena forte inico 09-11-2022
+
 #primeiros comandos
+
 print('ola mundo')
 
 
--- 000.variacoes.eu.2.py
+++ 000.variacoes.eu.2.py
@@ -1,3 +1,7 @@
+#curso_python, william pena forte inico 09-11-2022
+
+#brincando com print.
+
 nome = input ('qual seu nome?')
 idade = input ('qual sua idade?')
 peso = input ('qual seu peso?')

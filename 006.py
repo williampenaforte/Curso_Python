89f98358c25b79e07efd0a7ae9a266e6ff4f129a
williampenaforte <williampenaforte@gmail.com>
2022-11-23 11:54:58 -0300

    231122

--- 006.py
+++ 006.py
@@ -10,4 +10,13 @@ n = float(input('digite um numero: '))
 print('O dobro de {} vale {}.'.format(n,(n*2)))
 print('O triplo de {} vale {}.\nA rais quadrade de {} e igual {:.2f}.'.format(n,(n*3),n,pow(n,(1/2))))
 
-#teste....
+# teste.. Mudei pra usar o visual studio code
+# Versão: 1.73.1 (user setup)
+# Commit: 6261075646f055b99068d3688932416f2346dd3b
+# Data: 2022-11-09T04:27:29.066Z
+# Electron: 19.0.17
+# Chromium: 102.0.5005.1673
+# Node.js: 16.14.2
+# V8: 10.2.154.15-electron.0
+# SO: Windows_NT x64 10.0.19044
+# Em área restrita: No

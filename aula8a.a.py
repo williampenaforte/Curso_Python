3f3b1e2f57fd347662ee4635ba873bf37565d202
william <williampenaforte@gmail.com>
2022-11-28 16:36:22 -0300

    aula8aa

---
+++ aula8a.a.py
@@ -0,0 +1,9 @@
+#exercios py william 28/11/2022
+
+#Primeiro exemplo importação.
+import math
+
+num = int(input('Digite um numero:. '))
+raiz = math.sqrt(num)
+
+print ('a raiz de {} é {}'.format(num,raiz))
--- aula8a.py
+++ aula8a.py
@@ -1,13 +1,5 @@
 #exercios py william 28/11/2022
 
-#-----------------------------------------------------------------------------------
-#Primeiro exemplo importação.
-#import math
-
-#num = int(input('Digite um numero:. '))
-#raiz = math.sqrt(num)
-
-#print ('a raiz de {} é {}'.format(num,raiz))
 
 #-----------------------------------------------------------------------------------
 #Segundo exemplo importação.

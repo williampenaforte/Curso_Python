28ce4e0dd9b1cc02544eecb56ddc8189dfc83fa7
williampenaforte <58314006+williampenaforte@users.noreply.github.com>
2022-11-28 08:59:21 -0300

    aula8

---
+++ aula8a.py
@@ -0,0 +1,36 @@
+
+#-----------------------------------------------------------------------------------
+#Primeiro exemplo importação.
+#import math
+
+#num = int(input('Digite um numero:. '))
+#raiz = math.sqrt(num)
+
+#print ('a raiz de {} é {}'.format(num,raiz))
+
+#-----------------------------------------------------------------------------------
+#Segundo exemplo importação.
+#import math
+
+#num = int(input('Digite um numero:. '))
+#raiz = math.sqrt(num)
+
+#print ('a raiz de {} é {}'.format(num, math.ceil(raiz) ))
+
+
+#-----------------------------------------------------------------------------------
+#Terceiro exemplo importação.
+from math import sqrt
+
+num = int(input('Digite um numero:. '))
+#raiz = math.sqrt(num) #torna-se dispensavel esta linha
+raiz = sqrt(num) #assim funciona.. com a linha acima nao funciona, pois mudou a forma como importar.. 
+#neste formato importamos apenas o "sqrt"...
+
+#print ('a raiz de {} é {}'.format(num, math.ceil(raiz) )) #tambem removido o ceil pois nao foi importado...
+print ('a raiz de {} é {} '.format(num,raiz))
+
+
+#-----------------------------------------------------------------------------------
+
+

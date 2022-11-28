7729a722f95acde4fa0677e651f61e7bc82b10bf
williampenaforte <williampenaforte@gmail.com>
2022-11-28 08:16:14 -0300

    ..

---
+++ 014.py
@@ -0,0 +1,10 @@
+#exercicios py, william 27-11-22
+
+#escreva um programa que converta uma temperatura digitada em °C e converta em °F 
+
+c = float(input('Qual a temperatura? '))
+
+t = ((9*c)/5)+32
+
+print('a temperatura de {}°C corresponde a {}°F! '.format(c,t))
+

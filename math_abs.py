794f0074059310159dafac4f9edb53fff4084eca
williampenaforte <williampenaforte@gmail.com>
2022-11-30 18:40:39 -0300

    ...

---
+++ math_abs.py
@@ -0,0 +1,9 @@
+#fonte https://www.w3schools.com/python/python_math.asp
+
+#A abs()função retorna o valor absoluto (positivo) do número especificado:
+
+import math
+
+x = abs(-7.25)
+
+print(x)
---
+++ math_pow.py
@@ -0,0 +1,16 @@
+#fonte https://www.w3schools.com/python/python_math.asp
+
+#A função retorna o valor de x à potência de y (x y ).pow(x, y)
+
+#fonte https://docs.python.org/pt-br/3.10/library/math.html
+
+#Retorna x elevado à potência y.
+#Os casos excepcionais seguem o Anexo ‘F’ da norma C99, tanto quanto possível.
+#Em particular, pow(1.0, x) e pow(x, 0.0) sempre retornam 1.0, mesmo quando x é um zero ou um NaN.
+#Se ambos x e y são finitos, x é negativo, e y não é um inteiro, então pow(x, y) é indefinido e levanta
+
+import math
+
+x = pow(4, 3)
+
+print(x)

80c4e95e1621e9e9cc584f2ed4840b05d51df90e
williampenaforte <williampenaforte@gmail.com>
2022-11-30 18:53:54 -0300

    ...

--- mat_ceil.py
+++ mat_ceil.py
@@ -1,8 +1,8 @@
 #O math.ceil()
-# método arredonda um número para cima até o inteiro mais próximo
+# método arredonda um número para cima até o inteiro mais próximo e retorna o resultado.
 
 import math
 
 x = math.ceil(1.4)
 
-print(x) # returns 2
+print(x)
--- math_floor.py
+++ math_floor.py
@@ -1,8 +1,8 @@
-#O math.floor() método arredonda um número para baixo até o inteiro mais próximo e
-# retorna o resultado:
+#O math.floor()
+# método arredonda um número para baixo até o inteiro mais próximo e retorna o resultado.
 
 import math
 
 y = math.floor(1.4)
 
-print(y) # returns 1
+print(y)

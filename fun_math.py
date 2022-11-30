6c40ccea2b895d199b4124de00a24cc4d8636695
williampenaforte <williampenaforte@gmail.com>
2022-11-30 18:20:01 -0300

    ...

--- fun_math.py
+++ fun_math.py
@@ -9,4 +9,14 @@
 #Built-in Math Functions
 #Funções matemáticas integradas
 #The min() and max() functions can be used to find the lowest or highest value in an iterable:
+
+#Primeiro Exemplo
 #As funções min()e max()podem ser usadas para encontrar o valor mais baixo ou mais alto em um iterável:
+
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)

3af490c1e14cf41eafc4d5956530c4a8014d95c6
williampenaforte <williampenaforte@gmail.com>
2022-11-30 18:32:15 -0300

    ...

---
+++ fun_math.py
@@ -0,0 +1,16 @@
+#fonte https://www.w3schools.com/python/python_math.asp
+
+#Python has a set of built-in math functions, including an extensive math module,
+#O Python possui um conjunto de funções matemáticas integradas, incluindo um extenso módulo matemático,
+
+#that allows you to perform mathematical tasks on numbers.
+#que permite realizar tarefas matemáticas com números.
+
+#Built-in Math Functions
+#Funções matemáticas integradas
+#The min() and max() functions can be used to find the lowest or highest value in an iterable:
+
+#Primeiro Exemplo
+#As funções min()e max()podem ser usadas para encontrar o valor mais baixo ou mais alto em um iterável:
+
+#siga os outros arquivos math...A...B....C...Etc
--- math_min_max.py
+++ math_min_max.py
@@ -10,4 +10,10 @@
 #Funções matemáticas integradas
 #The min() and max() functions can be used to find the lowest or highest value in an iterable:
 
-#siga os outros arquivos math...A...B....C...Etc
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)

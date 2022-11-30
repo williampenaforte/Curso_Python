a884e52a3baedb0aa27d9d410aadbe916bcffe5c
williampenaforte <williampenaforte@gmail.com>
2022-11-30 18:57:54 -0300

    ...

---
+++ math_acos.py
@@ -0,0 +1,10 @@
+#O math.acos()
+#Retorne o arco cosseno de diferentes números:
+
+import math
+
+print(math.acos(0.55))
+print(math.acos(-0.55))
+print(math.acos(0))
+print(math.acos(1))
+print(math.acos(-1))

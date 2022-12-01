7429e4c4f8c5bc5f4328f849e2efcdd3bdc4b6e3
williampenaforte <williampenaforte@gmail.com>
2022-12-01 12:07:25 -0300

    ...

--- 018.py
+++ 018.py
@@ -1,5 +1,17 @@
-#william pena forte desafios curso em video python 30/11/2022
+'''
 
-#faca um progama que leia um angulo qualquer e mostre na tela o valor do seno, cosseo e
-# tangente desse angulo.
-#dica ver todas as classes do modulo "math"
+william pena forte desafios curso em video python 30/11/2022
+
+faca um progama que leia um angulo qualquer e mostre na tela o valor do seno, cosseo e
+tangente desse angulo. Dica ver todas as classes do modulo "math"
+
+'''
+
+import math
+ângulo = float(input('Digite o ângulo que vc deseja '))
+seno = math.sin(math.radians(ângulo))
+print('O angulo de {} tem o SENO de {:.2F}'.format(ângulo,seno))
+cosseno = math.cos(math.radians(ângulo))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(ângulo,cosseno))
+tangente = math.tan(math.radians(ângulo))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(ângulo,tangente))
--- 019.py
+++ 019.py
@@ -2,3 +2,16 @@
 
 #um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 #Faça um programa que ajude ele , lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
+
+a = str(input('Nome Primeiro Aluno:. '))
+b = str(input('Nome Segundo Aluno:. '))
+c = str(input('Nome Terceiro Aluno:. '))
+d = str(input('Nome Quarto Aluno:. '))
+
+lista = [a,b,c,d]
+
+escolhido = random.choice(lista)
+
+print('O aluno escolhido foi {}'.format(escolhido))

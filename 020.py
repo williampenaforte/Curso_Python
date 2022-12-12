9799117745ac51db60d210d34f82b6fcbe533f0c
william <williampenaforte@gmail.com>
2022-12-12 10:16:38 -0300

    aula8a.e

--- 020.py
+++ 020.py
@@ -3,3 +3,9 @@
 #O mesmo professor do desafio anterior quer sortear a ordem de apresentação dos alunos.
 #Faça um programa que leia o nome dos quatro alunos e mostre a ordem soreada.
 
+import random
+n1 = str(input('Primeiro aluno'))
+n2 = str(input('Segundo aluno'))
+n3 = str(input('Terceiro aluno'))
+n4 = str(input('Quarto aluno'))
+
--- fun_math.py
+++ fun_math.py
@@ -1,10 +1,12 @@
 #fonte https://www.w3schools.com/python/python_math.asp
 
-#Python has a set of built-in math functions, including an extensive math module,
-#O Python possui um conjunto de funções matemáticas integradas, incluindo um extenso módulo matemático,
 
-#that allows you to perform mathematical tasks on numbers.
+#O Python possui um conjunto de funções matemáticas integradas, incluindo um extenso módulo matemático,
+#Python has a set of built-in math functions, including an extensive math module,
+
 #que permite realizar tarefas matemáticas com números.
+#that allows you to perform mathematical tasks on numbers.
+
 
 #Built-in Math Functions
 #Funções matemáticas integradas

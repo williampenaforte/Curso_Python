c8ea5aacb75adccf95f217c72f58e56a4b651fa1
william <williampenaforte@gmail.com>
2022-12-12 11:58:13 -0300

    12/12/2022

--- 021.py
+++ 021.py
@@ -1,8 +1,5 @@
-#william pena forte desafios curso em video python 30/11/2022
-
-#Faça um programa em python que abra e reproduza o audio de uma arquivo mp3
-
-#use modulos kkk...
+'''william pena forte desafios curso em video python 30/11/2022'''
+'''Faça um programa em python que abra e reproduza o audio de uma arquivo mp3'''
 
 import pygame
 pygame.init()

d57e8c2339aff3a20a036d72ac4a9bb7cec3f05d
william <williampenaforte@gmail.com>
2022-12-12 10:27:30 -0300

    aula8a.e

--- 020.py
+++ 020.py
@@ -4,8 +4,13 @@
 #Faça um programa que leia o nome dos quatro alunos e mostre a ordem soreada.
 
 import random
-n1 = str(input('Primeiro aluno'))
-n2 = str(input('Segundo aluno'))
-n3 = str(input('Terceiro aluno'))
-n4 = str(input('Quarto aluno'))
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
 
+lista = [n1,n2,n3,n4]
+
+random.shuffle(lista)
+print('A ordem da apresentação sera!')
+print(lista)

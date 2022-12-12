a2179c00a0327db442578b53569986268a83fe58
william <williampenaforte@gmail.com>
2022-12-12 10:31:44 -0300

    .

---
+++ 020.a.py
@@ -0,0 +1,16 @@
+#william pena forte desafios curso em video python 30/11/2022
+
+#O mesmo professor do desafio anterior quer sortear a ordem de apresentação dos alunos.
+#Faça um programa que leia o nome dos quatro alunos e mostre a ordem soreada.
+
+from random import shuffle
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+
+lista = [n1,n2,n3,n4]
+
+shuffle(lista)
+print('A ordem da apresentação sera!')
+print(lista)

482eb7ff49f1f0100719f32c1443f1f42fe0279a
williampenaforte <williampenaforte@gmail.com>
2022-12-15 16:37:39 -0300

    15-12-2022

---
+++ aula09_1.py
@@ -0,0 +1,19 @@
+frase = ('Curso em Video Python')
+#print (frase)
+#print(frase[3])
+#print(frase[3:13])
+#print(frase[:13])
+#print(frase[13:])
+#print(frase[1:15])
+#print(frase[1:15:2])#o ultimo fala pra pular de dois em dois
+'''print("""agora com um grande texto, guanabara do curso em video python encina como escrever
+um grande texto na tela sem fazer varios print 1, print 2 e tec... Bom eu programando colocaria numa variavel e 
+veria o que aconteceria... bom vou testar isso na proxima linha
+inscrevase no meu canal do youtube https://www.youtube.com/watch?v=a9PWmCZ7ieo esta e o canal dos loucos por
+trilha pmw """)'''
+novafraze = ('''agora com um grande texto, guanabara do curso em video python encina como escrever
+um grande texto na tela sem fazer varios print 1, print 2 e tec... Bom eu programando colocaria numa variavel e 
+veria o que aconteceria... bom vou testar isso na proxima linha
+inscrevase no meu canal do youtube https://www.youtube.com/watch?v=a9PWmCZ7ieo esta e o canal dos loucos por
+trilha pmw ''')
+print(novafraze)#deu a mesma coisa kkkk
